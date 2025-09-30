c315cb5b510ad80dc61b869aa3989bf83b5ab1f2
LetsBuildAFlyingDonkey <LetsBuildAFlyingDonkey@github.com>
2025-09-30 20:43:44 +0200

    Add switching logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import { loadHomepage } from "./home.js";
 import { loadMenu } from "./menu.js";
 import { loadAbout } from "./about.js";
 
-// loadHomepage();
 // loadMenu();
-loadAbout();
+// loadAbout();
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector(".menu");
+const aboutButton = document.querySelector(".about");
+const divContent = document.body.querySelector("#content");
+
+function switchToPage(pageToSwitch) {
+    divContent.innerHTML = "";
+    switch (pageToSwitch) {
+        case "home":
+            loadHomepage();
+            break;
+        case "menu":
+            loadMenu();
+            break;
+        case "about":
+            loadAbout();
+            break;
+    }
+}
+
+homeButton.addEventListener("click", (event) =>
+    switchToPage(event.target.className)
+);
+menuButton.addEventListener("click", (event) =>
+    switchToPage(event.target.className)
+);
+aboutButton.addEventListener("click", (event) =>
+    switchToPage(event.target.className)
+);
+
+loadHomepage();
